4d80df0e94f91f87b017c6f3d15e5b3694e87069
Matthew Van Dusen <imagineux&gmail.com>
2026-01-31 23:03:59 -0600

    Chore: Base Path guess

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/ui.ankhstudio.com/' : '/',
+  base: '/',
 
   root: __dirname,
 
   publicDir: 'public',
-
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/@ankh-studio/components/dist/ankh/*',
-          dest: 'ankh',
-        },
-      ],
-    }),
-  ],
 
   resolve: {
     alias: {
